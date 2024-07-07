98f2ad32e8d2f073e8460e7471d4e7cd64de07d7
sunnyau <sunny.au@outlook.com>
2024-07-07 23:43:38 +0100

    add CancelFutureTest

---
+++ src/test/java/completablefuture/CancelFutureTest.java
@@ -0,0 +1,50 @@
+package completablefuture;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * We use future.cancel() to interrupt a thread.
+ */
+public class CancelFutureTest {
+
+    @Test
+    public void futureCancelShouldInterruptThread() throws InterruptedException {
+        CountDownLatch waitLatch = new CountDownLatch(1);
+
+        CompletableFuture<?> future = CompletableFuture.runAsync(() -> {
+            try {
+                System.out.println("Wait");
+                waitLatch.await(); // cancel should interrupt
+                System.out.println("Done");
+            } catch (InterruptedException e) {
+                System.out.println("Interrupted");
+                throw new RuntimeException(e);
+            } catch (CancellationException e2) {
+                System.out.println("CancellationException is called");
+            }
+        });
+
+        // Thread.sleep(10); // give it some time to start (ugly, but works)
+
+        // the future is still running.
+        assertFalse(future.isCancelled());
+        assertFalse(future.isDone());
+
+        future.cancel(true);
+        System.out.println("Cancel called");
+
+        // the future is cancelled.
+        assertTrue(future.isCancelled());
+        assertTrue(future.isDone());
+
+        // Thread.sleep(100); // give it some time to finish
+    }
+
+}
